475a6b9f9b333ef814a855ad44b18b8beffb7bb8
Hannah-Scheuer <hscheuer20@gmail.com>
2020-12-09 13:09:03 -0500

    attempting to write the two constructors

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -1,3 +1,13 @@
 public class NoNullArrayList<T> extends ArrayList<T>{
+
+  public NoNullArrayList<T>(){
+    super(1);
+  }
+
+  public NoNullArrayList<T>(startingCap){
+    super(startingCap);
+  }
+
   
+
 }
