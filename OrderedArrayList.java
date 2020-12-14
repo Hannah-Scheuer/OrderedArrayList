9d85c9ecd81333d1f88ec6bf629fa3243c5d2700
Hannah-Scheuer <hscheuer20@gmail.com>
2020-12-13 21:49:50 -0500

    final edits

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -11,7 +11,7 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
 
   private int findIndex(T element){
     for (int i=0;i< size();i++){
-      if (element.compareTo(get(i))>0){
+      if (element.compareTo(get(i))<0){
         return i;
       }
     }
